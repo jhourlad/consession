bd36a01dcb938b88c3117dd98919c7af4a940bd1
Jhourlad Estrella <m@jhourlad.com>
2020-07-23 06:50:29 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# consession
+
+A simple session package for Flutter that works on mobile, web and (hopefully) desktop apps. 
+
+## Installation
+Coming soon.
+
+## Usage
+Coming soon.
---
+++ lib/consession.dart
@@ -0,0 +1,48 @@
+library consession;
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Consession {
+  Map _session;
+
+  SharedPreferences prefs;
+
+  SessionService() {}
+
+  Future _initSharedPrefs() async {
+    this.prefs = await SharedPreferences.getInstance();
+  }
+
+  Future get(key) async {
+    await _initSharedPrefs();
+    return this.prefs.get(key);
+  }
+
+  Future set(key, value) async {
+    await _initSharedPrefs();
+
+    switch(value.runtimeType) {
+      case String: {
+        this.prefs.setString(key, value);
+      } break;
+
+      case int: {
+        this.prefs.setInt(key, value);
+      } break;
+
+      case bool: {
+        this.prefs.setBool(key, value);
+      } break;
+
+      case double: {
+        this.prefs.setDouble(key, value);
+      } break;
+
+      case List: {
+        this.prefs.setStringList(key, value);
+      } break;
+    }
+
+    this._session[key] = value;
+  }
+}
